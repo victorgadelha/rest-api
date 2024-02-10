1b76367164f0ca69cc609369657fb4fa17182441
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-02-09 22:31:31 -0300

    feat: refatorando inicialização do servidor

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
+app.use("/teste", (req, res, next) => {
   res.status(200).send({
     mensagem: "OK, deu certo!",
   });
