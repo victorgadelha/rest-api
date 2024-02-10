5526429a4c054ac6d4131e961cdf79ec55bd15ae
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-02-09 23:00:49 -0300

    refactor: comentando rotas

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+// RETORNA TODOS OS PRODUTOS
 router.get('/', (req, res, next) => {
   res.status(200).send({
     mensagem: 'Usando o GET dentro da rota de produtos',
   });
 });
 
+// INSERE UM PRODUTO
 router.post('/', (req, res, next) => {
   res.status(200).send({
     mensagem: 'Usando o POST dentro da rota de produtos',
   });
 });
 
+// RETORNA OS DADOS DE UM PRODUTO
 router.get('/:id_produto', (req, res, next) => {
   const id = req.params.id_produto;
 
