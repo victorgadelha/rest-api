5503830e15c398a88f0d4b8e1402d69270129408
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-02-11 13:42:51 -0300

    feat: tratando a mensagem de erro

--- app.js
+++ app.js
@@ -12,9 +12,18 @@ app.use('/pedidos', rotaPedidos);
 
 // Quando não encontrar a rota
 app.use((req, res, next) => {
-  const erro = new Error('Não encontrado.');
-  erro.status(404);
-  next(erro);
+  const error = new Error('Não encontrado.');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    error: {
+      messagem: error.message,
+    },
+  });
 });
 
 app.use('/teste', (req, res, next) => {
