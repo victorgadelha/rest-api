a41ec263888bb73fe077f8367f6b0a2430ff6e28
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-02-11 13:44:58 -0300

    feat: instalando e configurando o bodyparser

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extends: false }));
+app.use(bodyParser.json());
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
